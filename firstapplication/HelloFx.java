2dbb9577ba4ada0b0bca4084ff12f05ae909cbfd
Sayed606Dawodi <134128886+Sayed606Dawodi@users.noreply.github.com>
2023-05-20 18:10:58 -0700

    Clinic Management System (Java Swing)

---
+++ firstapplication/HelloFx.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package firstapplication;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author sayed
+ */
+public class HelloFx extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+    }
+    public static void main(String[] args){
+        launch(args);
+    }
+    
+}
